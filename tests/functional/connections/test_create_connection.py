79f2d6ef74b11b9ddc5eb51e09f9903dfc8274f5
Vianney Aubrecht <jv.aubrecht@gmail.com>
2019-05-08 13:44:04 -0300

    test_create_connection update and functional validation tests

--- tests/functional/connections/test_create_connection.py
+++ tests/functional/connections/test_create_connection.py
@@ -1,28 +1,60 @@
 from http import HTTPStatus
 
+import pytest
+
 from tests.factories import PersonFactory
 
 from connections.models.connection import Connection
 
 
-def test_can_create_connection(db, testapp):
+@pytest.fixture
+def connection_payload(db):
     person_from = PersonFactory(first_name='Diana')
     person_to = PersonFactory(first_name='Harry')
     db.session.commit()
-    payload = {
-        'from_person_id': person_from.id,
-        'to_person_id': person_to.id,
+    return {
         'connection_type': 'mother',
+        'from_person_id': person_from.id,
+        'to_person_id': person_to.id,        
     }
-    res = testapp.post('/connections', json=payload)
+
+
+def test_can_create_connection(db, testapp, connection_payload):
+
+    res = testapp.post('/connections', json=connection_payload)
 
     assert res.status_code == HTTPStatus.CREATED
 
+    for field in connection_payload:
+        assert res.json[field] == connection_payload[field]
     assert 'id' in res.json
 
     connection = Connection.query.get(res.json['id'])
 
     assert connection is not None
-    assert connection.from_person_id == person_from.id
-    assert connection.to_person_id == person_to.id
-    assert connection.connection_type.value == 'mother'
+    for field in connection_payload:
+        if field == 'connection_type':
+            assert connection.connection_type.value == connection_payload[field]
+        else:
+            assert getattr(connection, field) == connection_payload[field]
+
+#Note: Passing - However, foobar enum method requires deeper analysis
+@pytest.mark.parametrize('field, value, error_message', [
+    pytest.param('from_person_id', None, 'Field may not be null.', id='missing from person connection'),
+    pytest.param('to_person_id', None, 'Field may not be null.', id='missing to person connection'),
+    pytest.param('connection_type', None, 'Field may not be null.', id='missing connection_type'),
+    pytest.param('connection_type', 'foobar', 'Invalid enum member foobar', id='invalid connection_type'),
+])
+
+
+def test_create_connection_validations(db, testapp, connection_payload, field, value, error_message):
+
+    connection_payload[field] = value
+    res = testapp.post('/connections', json=connection_payload)
+
+    assert res.status_code == HTTPStatus.BAD_REQUEST
+    assert res.json['description'] == 'Input failed validation.'
+    errors = res.json['errors']
+    assert error_message in errors[field]            
+
+
